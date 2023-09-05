4b5de522f1b36658b199a6fa299f08e0face20bf
Wassim Ben R <wassimbenr@gmail.com>
2023-09-05 23:29:14 +0100

    fixing some problem

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { LandingPage } from "./Components/Landing_Page/LandingPage";
 import Login from "./Components/Login/Login";
 import SignUp from "./Components/Sign_Up/Sign_Up";
 import Navbar from "./Components/Navbar/Navbar";
-import InstantConsultation from "./Components/InstantConsultation/InstantConsultation";
 import FindDoctorSearch from "./Components/FindDoctorSearch/FindDoctorSearch";
 import DoctorBook from "./Components/DoctorCard/DoctorBook";
+import InstantConsultation from "./Components/InstantConsultationBooking/InstantConsultation";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/instant-consultation" element={<InstantConsultation />} />
